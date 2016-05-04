29407fe374a2b02abfc3eb5a42c67ae48ab5b68c
Branko Vukelic <branko@outernet.is>
2016-05-04 17:55:33 +0200

    Improve shell login experience
    This patch adds changes that improve shell login experience:
    
    - Disables root login and provides outernet user that is used instead
    - Enables sudo for outernet user
    - Adds colored shell prompts for root (red) and other users (green)
    - Adds MOTD that shows platform version and build and copyright notice
    - Adds outernet user home directory with README
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

---
+++ rxos/rootfs/etc/profile.d/ps1.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Provide usable colorized PS1 prompt.
+#
+# This file is part of rxOS.
+# rxOS is free software licensed under the
+# GNU GPL version 3 or any later version.
+#
+# (c) 2016 Outernet Inc
+# Some rights reserved.
+
+if [ "$PS1" ]; then
+  if [ "$(id -u)" -eq 0 ]; then
+    export PS1='[rxOS]\[\033[31m\][\u@\h:\w]# \[\033[0m\]'
+  else
+    export PS1='[rxOS]\[\033[32m\][\u@\h:\w]$ \[\033[0m\]'
+  fi
+fi
