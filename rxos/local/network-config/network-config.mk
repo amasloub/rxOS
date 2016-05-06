7c09f0b9be5c1de04b97fb108df0732d56449cc0
Branko Vukelic <branko@outernet.is>
2016-05-06 13:03:33 +0200

    Add network configuration as local package
    This patch adds a local package that contains network configuration files and
    init scripts, and exposes network configuration options via Kconfig. This
    allows us to quickly set network configuration options via the menu rather than
    editing the same information in multiple files. The rootfs overlay equivalents
    are removed.
    
    Currently, only the options for access point configuration are available.
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

---
+++ rxos/local/network-config/network-config.mk
@@ -0,0 +1,34 @@
+################################################################################
+#
+# network-config
+#
+################################################################################
+
+NETWORK_CONFIG_VERSION = 1.0
+NETWORK_CONFIG_LICENSE = GPL
+NETWORK_CONFIG_SITE = $(BR2_EXTERNAL)/local/network-config/src
+NETWORK_CONFIG_SITE_METHOD = local
+
+NETWORK_CONFIG_AP_IF = $(call qstrip,$(BR2_NETWORK_CONFIG_AP_IF))
+NETWORK_CONFIG_AP_IP = $(call qstrip,$(BR2_NETWORK_CONFIG_AP_IP))
+NETWORK_CONFIG_AP_NAME = $(call qstrip,$(BR2_NETWORK_CONFIG_AP_NAME))
+NETWORK_CONFIG_DHCP_START = $(call qstrip,$(BR2_NETWORK_CONFIG_DHCP_START))
+NETWORK_CONFIG_DHCP_END = $(call qstrip,$(BR2_NETWORK_CONFIG_DHCP_END))
+NETWORK_CONFIG_NETMASK = $(call qstrip,$(BR2_NETWORK_CONFIG_NETMASK))
+NETWORK_CONFIG_TAG = $(call qstrip,$(BR2_NETWORK_CONFIG_TAG))
+
+define NETWORK_CONFIG_INSTALL_TARGET_CMDS
+	$(INSTALL) -Dm644 $(@D)/hostapd.conf $(TARGET_DIR)/etc/hostapd.conf
+	$(INSTALL) -Dm644 $(@D)/dnsmasq.conf $(TARGET_DIR)/etc/dnsmasq.conf
+	$(INSTALL) -Dm644 $(@D)/interfaces $(TARGET_DIR)/etc/network/interfaces.rxos
+	$(INSTALL) -Dm755 $(@D)/ifplugd.action $(TARGET_DIR)/etc/ifplugd.action
+	$(INSTALL) -Dm755 $(@D)/dhcpcfg $(TARGET_DIR)/sbin/dhcpcfg
+	$(INSTALL) -Dm755 $(@D)/S81hostapd $(TARGET_DIR)/etc/init.d/S81hostapd
+	
+	sed -i 's|%IFACE%|$(NETWORK_CONFIG_AP_IF)|;s|%SSID%|$(NETWORK_CONFIG_AP_NAME)|;s|%IP%|$(NETWORK_CONFIG_AP_IP)|;s|%START%|$(NETWORK_CONFIG_DHCP_START)|;s|%END%|$(NETWORK_CONFIG_DHCP_END)|;s|%NETMASK%|$(NETWORK_CONFIG_NETMASK)|;s|%TAG%|$(NETWORK_CONFIG_TAG)|' \
+		$(TARGET_DIR)/etc/network/interfaces.rxos \
+		$(TARGET_DIR)/etc/hostapd.conf \
+		$(TARGET_DIR)/etc/dnsmasq.conf
+endef
+
+$(eval $(generic-package))
