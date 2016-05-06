d1dc0dfc8eaf0e3f72182ce4c642ad15dc2319d7
Branko Vukelic <branko@outernet.is>
2016-05-06 13:00:57 +0200

    Add support for .rxos override files
    This patch allows for files that end in .rxos to be used as override files by
    packages. The override files are used in situations where buildroot installs
    files into the root filesystem *after* the packages install them. To prevent
    package files from being overwritten, we can use .rxos files that will later be
    renamed to correct path by the post-build hook in this patch.
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

---
+++ rxos/scripts/rxos_overrides.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Rename files that end in .rxos extenion to names without the extension
+#
+# Some of the local packages will install files that end in .rxos. These files
+# are meant to replaces files that are added by Buildroot *after* packages are
+# installed, that would normally be overwritten if installed with default 
+# names.
+#
+# This script should be run as a post-build hook.
+#
+# This file is part of rxOS.
+# rxOS is free software licensed under the
+# GNU GPL version 3 or any later version.
+# 
+# (c) 2016 Outernet Inc
+# Some rights reserved.
+
+set -e
+
+SCRIPTDIR=$(dirname $0)
+. $SCRIPTDIR/helpers.sh
+
+for f in $(find "$TARGET_DIR" -name *.rxos); do
+  msg "Renaming '$f'"
+  mv "$f" "$(echo "$f" | sed 's/\.rxos//')"
+done
