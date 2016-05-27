bf2b02ee5cbccfbfe7861ec4514e316c53f59446
Branko Vukelic <branko@outernet.is>
2016-05-27 09:30:10 +0200

    Add global storage settings
    This commit introduces two new global settings that all packages using download
    directories should obey:
    
    - BR2_STORAGE_PRIMARY (internal storage)
    - BR2_STORAGE_SECONDARY (external storage)
    
    These are defined under System settings in the user-provided options section,
    and the packages submodule was updated to the version that supports these two
    settings.
    
    This change also affects self-partition boot hook, and FSAL, Librarian, and
    Lighttpd coniguration.
    
    Additionally, FSAL and LFTP blacklist settings have been updated to use the
    global BR2_PATH_BLACKLIST setting, which provides the regexp patterns for the
    path blacklists.
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rxos/local/fsal-config/fsal-config.mk
+++ rxos/local/fsal-config/fsal-config.mk
@@ -9,17 +9,21 @@ FSAL_CONFIG_LICENSE = GPL
 FSAL_CONFIG_SITE = $(BR2_EXTERNAL)/local/fsal-config/src
 FSAL_CONFIG_SITE_METHOD = local
 
+LISTIFY = $(BR2_EXTERNAL)/scripts/listify.sh
+BLACKLIST = $(foreach rxp,$(call qstrip,$(BR2_PATH_BLACKLIST)),'$(rxp)')
+FSAL_SED_CMDS += s|%BLACKLIST%|$(shell $(LISTIFY) $(BLACKLIST))|g;
+BASEDIRS = $(call qstrip,$(BR2_STORAGE_PRIMARY))
+BASEDIRS += $(call qstrip,$(BR2_STORAGE_SECONDARY))
+
 FSAL_SED_CMDS += s|%SOCKETPATH%|$(call qstrip,$(BR2_FSAL_SOCKETPATH))|g;
 FSAL_SED_CMDS += s|%LOGPATH%|$(call qstrip,$(BR2_FSAL_LOGPATH))|g;
 FSAL_SED_CMDS += s|%LOGSIZE%|$(call qstrip,$(BR2_FSAL_LOGSIZE))|g;
 FSAL_SED_CMDS += s|%LOGBACKUPS%|$(call qstrip,$(BR2_FSAL_LOGBACKUPS))|g;
-
-LISTIFY = $(BR2_EXTERNAL)/scripts/listify.sh
+FSAL_SED_CMDS += s|%BLACKLIST%|$(shell $(LISTIFY) $(BLACKLIST))|g;
+FSAL_SED_CMDS += s|%BASEDIRS%|$(shell $(LISTIFY) $(BASEDIRS))|g;
 
 define FSAL_CONFIG_INSTALL_TARGET_CMDS
-	sed -i '$(FSAL_SED_CMDS)' $(@D)/fsal.ini
-	sed -i "s|%BASEDIRS%|$$($(LISTIFY) $(call qstrip,$(BR2_FSAL_BASEDIRS)))|" \
-		$(@D)/fsal.ini
+	$(SED) '$(FSAL_SED_CMDS)' $(@D)/fsal.ini
 	$(INSTALL) -Dm644 $(@D)/fsal.ini $(TARGET_DIR)/etc/fsal.ini
 endef
 
--- rxos/local/librarian-config/librarian-config.mk
+++ rxos/local/librarian-config/librarian-config.mk
@@ -9,6 +9,9 @@ LIBRARIAN_CONFIG_LICENSE = GPL
 LIBRARIAN_CONFIG_SITE = $(BR2_EXTERNAL)/local/librarian-config/src
 LIBRARIAN_CONFIG_SITE_METHOD = local
 
+LISTIFY = $(BR2_EXTERNAL)/scripts/listify.sh
+BLACKLIST = $(foreach rxp,$(call qstrip,$(BR2_PATH_BLACKLIST)),'$(rxp)')
+
 LIBRARIAN_SED_COMMANDS += s|%ADDR%|$(call qstrip,$(BR2_LIBRARIAN_ADDR))|g;
 LIBRARIAN_SED_COMMANDS += s|%PORT%|$(call qstrip,$(BR2_LIBRARIAN_PORT))|g;
 LIBRARIAN_SED_COMMANDS += s|%DEFROUTE%|$(call qstrip,$(BR2_LIBRARIAN_DEFROUTE))|g;
@@ -20,8 +23,9 @@ LIBRARIAN_SED_COMMANDS += s|%FSAL_SOCKET%|$(call qstrip,$(BR2_FSAL_SOCKETPATH))|
 LIBRARIAN_SED_COMMANDS += s|%FSAL_LOGPATH%|$(call qstrip,$(BR2_FSAL_LOGPATH))|g;
 LIBRARIAN_SED_COMMANDS += s|%DHCP_START%|$(call qstrip,$(BR2_NETWORK_CONFIG_DHCP_START))|g;
 LIBRARIAN_SED_COMMANDS += s|%DHCP_END%|$(call qstrip,$(BR2_NETWORK_CONFIG_DHCP_END))|g;
-
-LISTIFY = $(BR2_EXTERNAL)/scripts/listify.sh
+LIBRARIAN_SED_COMMANDS += s|%PRIMARY%|$(call qstrip,$(BR2_STORAGE_PRIMARY))|g;
+LIBRARIAN_SED_COMMANDS += s|%SECONDARY%|$(call qstrip,$(BR2_STORAGE_SECONDARY))|g;
+LIBRARIAN_SED_COMMANDS += s|%BLACKLIST%|$(shell $(LISTIFY) $(BLACKLIST))|g;
 
 define LIBRARIAN_CONFIG_INSTALL_TARGET_CMDS
 	$(INSTALL) -Dm644 $(@D)/librarian.ini $(TARGET_DIR)/etc/librarian.ini
@@ -33,10 +37,10 @@ $(eval $(generic-package))
 # any components that may provide additional $LIBRARIAN_SED_COMMANDS for the 
 # configuration patch.
 define PATCH_LIBRARIAN_CONFIG
-	sed -i '$(LIBRARIAN_SED_COMMANDS)' $(TARGET_DIR)/etc/librarian.ini
-	sed -i "s|%COMPONENTS%|$$($(LISTIFY) $(LIBRARIAN_COMPONENTS))|" \
+	$(SED) '$(LIBRARIAN_SED_COMMANDS)' $(TARGET_DIR)/etc/librarian.ini
+	$(SED) 's|%COMPONENTS%|$(shell $(LISTIFY) $(LIBRARIAN_COMPONENTS))|' \
 		$(TARGET_DIR)/etc/librarian.ini
-	sed -i "s|%MENU%|$$($(LISTIFY) $(LIBRARIAN_MENU))|" \
+	$(SED) 's|%MENU%|$(shell $(LISTIFY) $(LIBRARIAN_MENU))|' \
 		$(TARGET_DIR)/etc/librarian.ini
 endef
 
