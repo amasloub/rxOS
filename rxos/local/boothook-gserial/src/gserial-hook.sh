6ae9c8bdd279a2548c6ab2a190781a20e5f509d2
Branko Vukelic <branko@outernet.is>
2016-06-29 20:49:19 +0200

    WIP Attempt to get gadget serial going
    Signed-off-by: Branko Vukelic <branko@outernet.is>

---
+++ rxos/local/boothook-gserial/src/gserial-hook.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Activate the gadget serial driver.
+#
+# This file is part of rxOS.
+# rxOS is free software licensed under the
+# GNU GPL version 3 or any later version.
+# 
+# (c) 2016 Outernet Inc
+# Some rights reserved.
+
+modprobe g_cdc
