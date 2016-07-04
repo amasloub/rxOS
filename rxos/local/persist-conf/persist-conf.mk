a2c2acb1fdaf2fea686cfa319bd2d74ba815922b
Branko Vukelic <branko@outernet.is>
2016-07-04 17:37:28 +0200

    Make persistent config more dynamic
    Using a flat file containing persistent files list does not yield well to
    programmatic modification of that list by various packages. This commit
    switches the persistent file list to a make variable so that it can be
    overridden by packages. The network-config package is also updated to take
    advantage of this feature.
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rxos/local/persist-conf/persist-conf.mk
+++ rxos/local/persist-conf/persist-conf.mk
@@ -9,13 +9,23 @@ PERSIST_CONF_LICENSE = GPLv3+
 PERSIST_CONF_SITE = $(BR2_EXTERNAL)/local/persist-conf/src
 PERSIST_CONF_SITE_METHOD = local
 
-PERSISTENT_CONF_LIST = $(call qstrip,$(BR2_PERSISTENT_CONF_LIST))
+PERSISTENT_CONF_LIST += $(call qstrip,$(BR2_PERSISTENT_CONF_LIST))
 PERSISTENT_CONF_DIR = $(call qstrip,$(BR2_PERSISTENT_CONF_DIR))
 
 define PERSIST_CONF_INSTALL_TARGET_CMDS
 	sed -i 's|%CONFDIR%|$(PERSISTENT_CONF_DIR)|g' $(@D)/persist.sh
 	$(INSTALL) -Dm755 $(@D)/persist.sh $(TARGET_DIR)/etc/setup.d/persist.sh
-	$(INSTALL) -Dm644 $(PERSISTENT_CONF_LIST) $(TARGET_DIR)/etc/persist.conf
 endef
+
+define PERSIST_CONF_INSTALL_LIST_CMDS
+	@$(call MESSAGE,"Installing persist.conf")
+	printf '$(foreach path,$(PERSISTENT_CONF_LIST),$(path)\n)' \
+		| sed 's/^ //' \
+		> $(TARGET_DIR)/etc/persist.conf
+endef
+
+ifeq ($(BR2_PACKAGE_PERSIST_CONF),y)
+TARGET_FINALIZE_HOOKS += PERSIST_CONF_INSTALL_LIST_CMDS
+endif
 
 $(eval $(generic-package))
