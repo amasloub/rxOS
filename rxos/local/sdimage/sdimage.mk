b156fcb2cdb565a9eec10970bae120c0c6a955e9
Branko Vukelic <branko@outernet.is>
2016-08-10 15:37:47 +0200

    Add target to rebuild the sd card image
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rxos/local/sdimage/sdimage.mk
+++ rxos/local/sdimage/sdimage.mk
@@ -9,6 +9,12 @@ SDIMAGE_LICENSE = GPL
 SDIMAGE_SITE = $(BR2_EXTERNAL)/local/sdimage/src
 SDIMAGE_SITE_METHOD = local
 
+.PHONY: rebuild-sdimage
+
 ifeq ($(BR2_PACKAGE_SDIMAGE),y)
 BR2_ROOTFS_POST_IMAGE_SCRIPT += $(call lpkgdir,sdimage)/src/mksdimage.sh
 endif
+
+rebuild-sdimage:
+	$(EXTRA_ENV) $(call lpkgdir,sdimage)/src/mksdimage.sh $(BINARIES_DIR) \
+		$(call qstrip,$(BR2_ROOTFS_POST_SCRIPT_ARGS))$(sep))
