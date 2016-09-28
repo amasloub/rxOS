97e81d6947e265e3f44dec91cc083cfc34ed1100
Andrean Franc <andrean.franc@gmail.com>
2016-09-28 18:51:03 +0200

    Add opakwatch package
    opakwatch installs a utility script into /usr/bin/opakwatch and an init
    script into /etc/init.d/S99opakwatch. When started the script will watch
    a specified directory (using inotify) for newly created files and attempt
    to extract them with tar to an also specified destination directory.

---
+++ rxos/local/opakwatch/src/opakwatch.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Watch the specified source folder for newly created files and
+# extract them as soon as they appear.
+#
+# This file is part of rxOS.
+# rxOS is free software licensed under the
+# GNU GPL version 3 or any later version.
+#
+# (c) 2016 Outernet Inc
+# Some rights reserved.
+USAGE='opakwatch SOURCE DESTINATION'
+TAR='/bin/tar'
+
+if [ $# != 2 ]
+then
+  echo >&2 "$USAGE"
+  exit 1
+fi
+
+SOURCE="$1"
+DESTINATION="$2"
+
+inotifywait -m "$SOURCE" --format '%w%f' -e create |
+  while read -r filename; do
+    echo "Discovered '$filename', attempting extraction..."
+    $TAR xf "$filename" --directory "$DESTINATION"
+  done
