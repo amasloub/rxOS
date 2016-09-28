4a75f2ab40b2492b517d8e827dca1d9e4f3f259d
Branko Vukelic <branko@outernet.is>
2016-09-28 14:56:33 +0200

    Fix fsal to not use old basepaths
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rxos/local/fsal-config/fsal-config.mk
+++ rxos/local/fsal-config/fsal-config.mk
@@ -4,22 +4,19 @@
 #
 ################################################################################
 
-FSAL_CONFIG_VERSION = 1.1
+FSAL_CONFIG_VERSION = 1.2
 FSAL_CONFIG_LICENSE = GPL
 FSAL_CONFIG_SITE = $(BR2_EXTERNAL)/local/fsal-config/src
 FSAL_CONFIG_SITE_METHOD = local
 
 FSAL_BLACKLIST = $(foreach rxp,$(call qstrip,$(BR2_PATH_BLACKLIST)),\n  $(rxp))
 FSAL_SED_CMDS += s|%BLACKLIST%|$(FSAL_BLACKLIST)|g;
-BASEDIRS = $(call qstrip,$(BR2_STORAGE_PRIMARY))
-BASEDIRS += $(call qstrip,$(BR2_STORAGE_SECONDARY))
 
 FSAL_SED_CMDS += s|%SOCKETPATH%|$(call qstrip,$(BR2_FSAL_SOCKETPATH))|g;
 FSAL_SED_CMDS += s|%LOGPATH%|$(call qstrip,$(BR2_FSAL_LOGPATH))|g;
 FSAL_SED_CMDS += s|%LOGSIZE%|$(call qstrip,$(BR2_FSAL_LOGSIZE))|g;
 FSAL_SED_CMDS += s|%LOGBACKUPS%|$(call qstrip,$(BR2_FSAL_LOGBACKUPS))|g;
 FSAL_SED_CMDS += s|%BLACKLIST%|$(FSAL_BLACKLIST)|g;
-FSAL_SED_CMDS += s|%BASEDIRS%|$(foreach path,$(BASEDIRS),\n  $(path))|g;
 
 define FSAL_CONFIG_INSTALL_TARGET_CMDS
 	$(SED) '$(FSAL_SED_CMDS)' $(@D)/fsal.ini
