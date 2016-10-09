0751ea84c8a2956403498ba37bf7d21b4b6dbb31
abhishek <abhishek@outernet.is>
2016-10-09 19:00:39 +0530

    add incrond service

---
+++ rxos/local/incron-service/incronservice.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# incron-service
+#
+################################################################################
+
+INCRONSERVICE_VERSION = 1.0
+INCRONSERVICE_LICENSE = GPLv3+
+INCRONSERVICE_SITE = $(BR2_EXTERNAL)/local/incron-service/src
+INCRONSERVICE_SITE_METHOD = local
+
+
+define INCRONSERVICE_INSTALL_INIT_SYSV
+	$(INSTALL) -Dm0644 $(@D)/incron.conf $(TARGET_DIR)/etc/incron.conf
+	$(INSTALL) -Dm0755 $(@D)/S99incrond   $(TARGET_DIR)/etc/init.d/S99incrond
+endef
+
+$(eval $(generic-package))
