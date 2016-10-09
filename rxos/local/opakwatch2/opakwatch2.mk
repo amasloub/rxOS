062a70abafa0953bc3bbb752dda61e0db0546a1a
abhishek <abhishek@outernet.is>
2016-10-09 22:20:31 +0530

    clean up opakwatch2 package

--- rxos/local/opakwatch2/opakwatch2.mk
+++ rxos/local/opakwatch2/opakwatch2.mk
@@ -13,7 +13,7 @@ OPAKWATCH2_SED_CMDS += s|%OPAKSOURCE%|$(call qstrip,$(BR2_OPAKWATCH2_SOURCE))|;
 OPAKWATCH2_SED_CMDS += s|%OPAKDESTINATION%|$(call qstrip,$(BR2_OPAKWATCH2_DESTINATION))|;
 
 define OPAKWATCH2_INSTALL_TARGET_CMDS
-	$(INSTALL) -Dm755 $(@D)/opakhandler.sh $(TARGET_DIR)/usr/bin/opakhandler.sh
+	$(INSTALL) -Dm755 $(@D)/opakhandler $(TARGET_DIR)/usr/bin/opakhandler
 	$(INSTALL) -Dm0644 $(@D)/opaks.incron $(TARGET_DIR)(call qstrip,$(BR2_INCRON_CONFDIR))/opaks.incron
 	$(SED) '$(OPAKWATCH2_SED_CMDS)' $(TARGET_DIR)(call qstrip,$(BR2_INCRON_CONFDIR))/opaks.incron
 endef
