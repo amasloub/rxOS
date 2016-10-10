b256f9e3dbdabfc5365878200ce0e65c1aa39388
abhishek <abhishek@outernet.is>
2016-10-10 18:16:21 +0530

    fix incron-service build issue due to name mismatch     package name and .mk name must match, and - converts to _ in the symbols

--- rxos/local/incron-service/incron-service.mk
+++ rxos/local/incron-service/incron-service.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-INCRONSERVICE_VERSION = 1.0
-INCRONSERVICE_LICENSE = GPLv3+
-INCRONSERVICE_SITE = $(BR2_EXTERNAL)/local/incron-service/src
-INCRONSERVICE_SITE_METHOD = local
+INCRON_SERVICE_VERSION = 1.0
+INCRON_SERVICE_LICENSE = GPLv3+
+INCRON_SERVICE_SITE = $(BR2_EXTERNAL)/local/incron-service/src
+INCRON_SERVICE_SITE_METHOD = local
 
 
-define INCRONSERVICE_INSTALL_INIT_SYSV
+define INCRON_SERVICE_INSTALL_INIT_SYSV
 	$(INSTALL) -Dm0755 $(@D)/S99incrond   $(TARGET_DIR)/etc/init.d/S99incrond
 endef
 
