d2e495cdf74881bb2810cbd56d4f1e52d847a8c1
abhishek <abhishek@outernet.is>
2016-10-11 06:50:03 +0530

    fix missing incron service conf dir

--- rxos/local/incron-service/incron-service.mk
+++ rxos/local/incron-service/incron-service.mk
@@ -12,6 +12,7 @@ INCRON_SERVICE_SITE_METHOD = local
 
 define INCRON_SERVICE_INSTALL_INIT_SYSV
 	$(INSTALL) -Dm0755 $(@D)/S99incrond   $(TARGET_DIR)/etc/init.d/S99incrond
+    $(INSTALL) -m755 -d $(TARGET_DIR)$(call qstrip,$(BR2_INCRON_SERVICE_CONFDIR))
 endef
 
 $(eval $(generic-package))
