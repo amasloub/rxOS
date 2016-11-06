7a4e6298a5486943e2843b362568c7f69b75a50b
abhishek <abhishek@outernet.is>
2016-11-06 22:16:49 +0530

    add node_modules cleanup

--- rxos/local/ui2/ui2.mk
+++ rxos/local/ui2/ui2.mk
@@ -4,12 +4,14 @@
 #
 ################################################################################
 
-UI2_VERSION = 0.33
+UI2_VERSION = 0.34
 UI2_SITE = https://github.com/Outernet-Project/RxOS-UI2
 UI2_SITE_METHOD = git
 
 define UI2_INSTALL_TARGET_CMDS
     cp -dr --no-preserve=ownership $(@D)/target/* $(TARGET_DIR)
+# clean up node_modules
+    rm -rf $(TARGET_DIR)/usr/lib/node_modules/exif/test
 endef
 
 $(eval $(generic-package))
