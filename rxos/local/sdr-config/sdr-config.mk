bcaa451ff8fc922d40300d9d3ff3a6620c01d1a9
abhishek <abhishek@outernet.is>
2016-11-08 03:55:11 +0530

    remove librarian remove fsal remove lighttpd remove sqlite fix sdrargs to pure shell (no python) disable telemetryd as it depends on python

--- rxos/local/sdr-config/sdr-config.mk
+++ rxos/local/sdr-config/sdr-config.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SDR_CONFIG_VERSION = 1.1
+SDR_CONFIG_VERSION = 1.2
 SDR_CONFIG_LICENSE = GPLv3+
 SDR_CONFIG_SITE = $(BR2_EXTERNAL)/local/sdr-config/src
 SDR_CONFIG_SITE_METHOD = local
@@ -13,7 +13,7 @@ SDR_CONFIG_SED_CMDS += s|%BINPATH%|$(call qstrip,$(BR2_SDR_BINARY_PATH))|;
 SDR_CONFIG_SED_CMDS += s|%JSON_PATH%|$(call qstrip,$(BR2_LIBRARIAN_SETTINGS_FILE))|;
 
 define SDR_CONFIG_INSTALL_TARGET_CMDS
-	$(INSTALL) -Dm755 $(@D)/sdrargs.py $(TARGET_DIR)/usr/sbin/sdrargs
+	$(INSTALL) -Dm755 $(@D)/sdrargs $(TARGET_DIR)/usr/sbin/sdrargs
 	$(INSTALL) -Dm755 $(@D)/ontimeout.sh $(TARGET_DIR)/usr/sbin/ontimeout
 	$(INSTALL) -Dm755 $(@D)/sdr.sh $(TARGET_DIR)/usr/sbin/sdr
 endef
