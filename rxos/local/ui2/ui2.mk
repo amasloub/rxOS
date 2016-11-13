e1dbf4766ad60d6896986a1c24bdfdb13e58580b
abhishek <abhishek@outernet.is>
2016-11-13 19:37:32 +0530

    pull ui2 v0.44

--- rxos/local/ui2/ui2.mk
+++ rxos/local/ui2/ui2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UI2_VERSION = 0.43
+UI2_VERSION = 0.44
 UI2_SITE = https://github.com/Outernet-Project/RxOS-UI2
 UI2_SITE_METHOD = git
 
