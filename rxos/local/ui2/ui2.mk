341dafebbcf578422d39804ea121c943037ed276
abhishek <abhishek@outernet.is>
2016-11-15 03:09:10 +0530

    pull "add transfers to tuner status -1" from ui2

--- rxos/local/ui2/ui2.mk
+++ rxos/local/ui2/ui2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UI2_VERSION = 0.47
+UI2_VERSION = 0.48
 UI2_SITE = https://github.com/Outernet-Project/RxOS-UI2
 UI2_SITE_METHOD = git
 
