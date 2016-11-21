2c9e93cb2eece39068fcb6a94002c0662f885ef7
abhishek <abhishek@outernet.is>
2016-11-21 19:03:20 +0530

    start UI2 service before ondd/sdr

--- rxos/local/ui2service/ui2service.mk
+++ rxos/local/ui2service/ui2service.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UI2SERVICE_VERSION = 0.2
+UI2SERVICE_VERSION = 0.3
 UI2SERVICE_SITE = $(BR2_EXTERNAL)/local/ui2service/src
 UI2SERVICE_SITE_METHOD = local
 
@@ -13,7 +13,7 @@ define UI2SERVICE_INSTALL_TARGET_CMDS
 endef
 
 define UI2SERVICE_INSTALL_INIT_SYSV
-	$(INSTALL) -Dm0755 $(@D)/S98ui2service $(TARGET_DIR)/etc/init.d/S98ui2service
+	$(INSTALL) -Dm0755 $(@D)/S70ui2service $(TARGET_DIR)/etc/init.d/S70ui2service
 endef
 
 $(eval $(generic-package))
