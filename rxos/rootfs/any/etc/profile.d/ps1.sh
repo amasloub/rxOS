6e33fd7cd20cfc6268c4bc6104d4383cda22667b
abhishek <abhishek@outernet.is>
2016-11-26 20:53:46 +0530

    add support for jumper to enable/disable no-autoboot-on-power     jumper between PD2 (pin 17 on U13 on CHIP) and GND: auto boot on power     no jumper: no auto boot on power, must press switch

--- rxos/rootfs/any/etc/profile.d/ps1.sh
+++ rxos/rootfs/any/etc/profile.d/ps1.sh
@@ -11,8 +11,8 @@
 
 if [ "$PS1" ]; then
   if [ "$(id -u)" -eq 0 ]; then
-    export PS1='[rxOS]\[\033[31m\][\u@\h:\w]# \[\033[0m\]'
+    export PS1='[Skylark]\[\033[31m\][\u@\h:\w]# \[\033[0m\]'
   else
-    export PS1='[rxOS]\[\033[32m\][\u@\h:\w]$ \[\033[0m\]'
+    export PS1='[Skylark]\[\033[32m\][\u@\h:\w]$ \[\033[0m\]'
   fi
 fi
