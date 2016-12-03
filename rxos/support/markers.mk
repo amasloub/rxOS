f2770a309e1cd0ba26e51815902e52c532c9f633
abhishek <abhishek@outernet.is>
2016-12-04 03:02:12 +0530

    fix timestamping

--- rxos/support/markers.mk
+++ rxos/support/markers.mk
@@ -8,7 +8,6 @@
 # Some rights reserved.
 
 RXOS_BUILD = $(shell cd $(BR2_EXTERNAL); git rev-parse --short HEAD)
-RXOS_TIMESTAMP = $(shell date -u --rfc-3339=seconds)
 RELEASE_FILE = $(TARGET_DIR)/etc/platform-release
 
 define GENERATE_MARKERS
@@ -17,7 +16,7 @@ define GENERATE_MARKERS
 	echo 'RXOS_SUBPLATFORM=$(RXOS_SUBPLATFORM)' > $(RELEASE_FILE)
 	echo 'RXOS_VERSION=$(RXOS_VERSION)' >> $(RELEASE_FILE)
 	echo 'RXOS_BUILD="$(RXOS_BUILD)"' >> $(RELEASE_FILE)
-	echo 'RXOS_TIMESTAMP="$(RXOS_TIMESTAMP)"' >> $(RELEASE_FILE)
+	echo 'RXOS_TIMESTAMP=$(RXOS_TIMESTAMP)' >> $(RELEASE_FILE)
 	echo '$(call qstrip,$(RXOS_VERSION))' > $(TARGET_DIR)/etc/version
 	echo '$(call qstrip,$(RXOS_PLATFORM))' > $(TARGET_DIR)/etc/platform
 	echo '$(call qstrip,$(RXOS_SUBPLATFORM))' > $(TARGET_DIR)/etc/subplatform
