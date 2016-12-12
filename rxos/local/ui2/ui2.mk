b8b621a051cc912f5ee29756f61c24a1136d1d7b
abhishek <abhishek@outernet.is>
2016-12-13 00:23:43 +0530

    pull in fixed ui2

--- rxos/local/ui2/ui2.mk
+++ rxos/local/ui2/ui2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UI2_VERSION = 0.66
+UI2_VERSION = 0.69
 UI2_SITE = https://github.com/Outernet-Project/RxOS-UI2
 UI2_SITE_METHOD = git
 
