4d855090f118195258c30fc88f6c4d4dbd0f17e0
abhishek <abhishek@outernet.is>
2016-12-29 19:54:42 +0530

    pull UI2: wikipedia app, news cleanup

--- rxos/local/ui2/ui2.mk
+++ rxos/local/ui2/ui2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UI2_VERSION = skylark-alpha-008
+UI2_VERSION = skylark-alpha-009
 UI2_SITE = https://github.com/Outernet-Project/RxOS-UI2
 UI2_SITE_METHOD = git
 
