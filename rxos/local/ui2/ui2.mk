2f859c34af117545a7a97bf5d8dc4453944b7afb
abhishek <abhishek@outernet.is>
2016-12-30 05:35:48 +0530

    pull ui2: rxos_config bug fix: failure on pristine config

--- rxos/local/ui2/ui2.mk
+++ rxos/local/ui2/ui2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UI2_VERSION = skylark-alpha-014
+UI2_VERSION = skylark-alpha-015
 UI2_SITE = https://github.com/Outernet-Project/RxOS-UI2
 UI2_SITE_METHOD = git
 
