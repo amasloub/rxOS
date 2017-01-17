d49910c031bc27accc31c19e7bf9fc693357a4ba
abhishek <abhishek@outernet.is>
2017-01-18 01:34:28 +0530

    pull ui2: network config fixes, related to country(CRDA) setting

--- rxos/local/ui2/ui2.mk
+++ rxos/local/ui2/ui2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UI2_VERSION = skylark-alpha-019
+UI2_VERSION = skylark-alpha-020
 UI2_SITE = https://github.com/Outernet-Project/RxOS-UI2
 UI2_SITE_METHOD = git
 
