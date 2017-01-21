f3e95cb20bfc8f75f6829e368b760df7bb76effd
abhishek <abhishek@outernet.is>
2017-01-21 18:26:15 +0530

    pull ui2: increase News app autoupdate to 30 minutes, bump skylark to 1.2

--- rxos/local/ui2/ui2.mk
+++ rxos/local/ui2/ui2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UI2_VERSION = skylark-alpha-022
+UI2_VERSION = skylark-alpha-023
 UI2_SITE = https://github.com/Outernet-Project/RxOS-UI2
 UI2_SITE_METHOD = git
 
