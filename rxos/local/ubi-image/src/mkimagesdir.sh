87bd4714c2744d732a812b5e28068d6c49939d2c
abhishek <abhishek@outernet.is>
2017-01-22 18:13:10 +0530

    remove conf_clean as old sop_handler doesn't support it (next will)

--- rxos/local/ubi-image/src/mkimagesdir.sh
+++ rxos/local/ubi-image/src/mkimagesdir.sh
@@ -167,7 +167,6 @@ part_cp zImage /boot
 mtd_nandwrite uboot.bin uboot
 part_cp sunxi-spl-with-ecc.bin.1664 /boot
 part_cp sunxi-spl-with-ecc.bin.1280 /boot
-conf_clean
 EOF
 
 if [ "$KEY_RELEASE" = "yes" ]
