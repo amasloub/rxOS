221641826faeb37ac8c67fa3102336c26ae50ff2
abhishek <abhishek@outernet.is>
2017-01-24 19:14:58 +0530

    pull ui2: upload api fix for guest user

--- rxos/local/ui2/ui2.mk
+++ rxos/local/ui2/ui2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UI2_VERSION = skylark-alpha-027
+UI2_VERSION = skylark-alpha-028
 
 UI2_SITE = https://github.com/Outernet-Project/RxOS-UI2
 UI2_SITE_METHOD = git
