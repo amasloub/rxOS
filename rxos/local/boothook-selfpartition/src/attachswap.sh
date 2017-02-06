356283b138bebf0c219e6b138a05d9265b2b49e1
abhishek <abhishek@outernet.is>
2017-02-06 17:23:11 +0530

    attach swap based on availability of mtdblock4 or mmcblk0p2     this allows common script to be used for both chip and dc

--- rxos/local/boothook-selfpartition/src/attachswap.sh
+++ rxos/local/boothook-selfpartition/src/attachswap.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
-mkswap /dev/mmcblk0p2
-swapon /dev/mmcblk0p2
+# nand storage
+if [ -e /dev/mtdblock4 ]
+then
+    mkswap /dev/mtdblock4
+    swapon /dev/mtdblock4
+fi
+
+# sdcard storage
+if [ -e /dev/mmcblk0p2 ]
+then
+    mkswap /dev/mmcblk0p2
+    swapon /dev/mmcblk0p2
+fi
