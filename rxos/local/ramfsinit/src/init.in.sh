29f97250226519c30d67d34381be89b141f2bdda
abhishek <abhishek@outernet.is>
2017-02-06 22:21:14 +0530

    fix unified ramfsinit

--- rxos/local/ramfsinit/src/init.in.sh
+++ rxos/local/ramfsinit/src/init.in.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 
-if [ -e /dev/mmcblk0p1 ]
+PATH=/bin
+
+CONSOLE=/dev/console
+
+# Set the date to a sane value
+date -u "2017-01-01 0:00:00"
+
+# Populate the /dev, /proc, and /sys directories
+mkdir -p /sys /proc /dev
+mount -t devtmpfs devtmpfs /dev
+mount -t proc proc /proc
+mount -t sysfs sysfs /sys
+
+# Setup console
+exec 0<$CONSOLE
+exec 1>$CONSOLE
+exec 2>$CONSOLE
+
+
+if  grep -q BOARD=dc /proc/cmdline
 then
     exec /init.dc
 else
