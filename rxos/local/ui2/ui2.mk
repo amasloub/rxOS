7982878c65b25be5adde6150fb9e8351ad6508af
abhishek <abhishek@outernet.is>
2017-02-18 19:30:11 +0530

    pull ui2: fixed build

--- rxos/local/ui2/ui2.mk
+++ rxos/local/ui2/ui2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UI2_VERSION = skylark-alpha-033
+UI2_VERSION = skylark-alpha-034
 
 UI2_SITE = https://github.com/Outernet-Project/RxOS-UI2
 UI2_SITE_METHOD = git
