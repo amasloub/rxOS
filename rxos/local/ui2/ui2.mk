67a019e96662465915e5b3d1bfd95b070abfcda0
abhishek <abhishek@outernet.is>
2017-02-19 20:31:10 +0530

    pull ui2: fix uri encode bug in News and Wikipedia, add opendoc support

--- rxos/local/ui2/ui2.mk
+++ rxos/local/ui2/ui2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UI2_VERSION = skylark-alpha-034
+UI2_VERSION = skylark-alpha-035
 
 UI2_SITE = https://github.com/Outernet-Project/RxOS-UI2
 UI2_SITE_METHOD = git
