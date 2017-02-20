e0e91060e56a58d133ec6dd62ab560f1b3c53a4a
abhishek <abhishek@outernet.is>
2017-02-21 02:11:35 +0530

    pull ui2: cleanup config, wifi config, versioned telemetry

--- rxos/local/ui2/ui2.mk
+++ rxos/local/ui2/ui2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UI2_VERSION = skylark-alpha-037
+UI2_VERSION = skylark-alpha-038
 
 UI2_SITE = https://github.com/Outernet-Project/RxOS-UI2
 UI2_SITE_METHOD = git
