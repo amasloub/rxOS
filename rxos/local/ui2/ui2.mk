629e5eaad5d80cd52c9a95f2ec492fdb0286a662
abhishek <abhishek@outernet.is>
2017-02-27 17:15:08 +0530

    pull ui2: fix value quoting in getconf

--- rxos/local/ui2/ui2.mk
+++ rxos/local/ui2/ui2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UI2_VERSION = skylark-alpha-047
+UI2_VERSION = skylark-alpha-048
 
 UI2_SITE = https://github.com/Outernet-Project/RxOS-UI2
 UI2_SITE_METHOD = git
